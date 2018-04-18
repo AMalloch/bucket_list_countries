e312d61df366661d3f39f1bf1074c720f32a4069
Angus Malloch <angusbass@hotmail.com>
2018-04-18 14:49:56 +0100

    Added a mongoclient with a db called bucket_list_db . Added a get method for a countries collection, this will return the selected collection as an array.

--- server.js
+++ server.js
@@ -10,6 +10,29 @@ app.use(bodyParser.urlencoded({extended: true}));
 // this will find any static file aka html css etc
 app.use(express.static("client/build"));
 
+const MongoClient = require("mongodb").MongoClient;
+
+MongoClient.connect("mongodb://localhost:27017", function (err, client) {
+  if(err){
+    console.log(err);
+    return;
+  }
+
+  const db = client.db("bucket_list_db");
+
+  app.get("/bucket_list", function(req, res){
+    const countriesCollection = db.collection("countries");
+    countriesCollection.find().toArray(function(err, countries){
+      if(err){
+        console.log(err);
+        res.status(500);
+        res.send();
+      }
+
+      res.json(countries);
+    });
+  })
+
  // listens at this port
   app.listen(3000, function(){
     console.log("Listening for requests on port 3000!");
