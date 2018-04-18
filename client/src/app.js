b5f4aaa7ac85e0f4059f98ffddc5a83c520854eb
daniel kelly <danieljkelly91@gmail.com>
2018-04-18 15:13:43 +0100

    adds request.js and requires request into app.js

--- client/src/app.js
+++ client/src/app.js
@@ -1,5 +1,10 @@
+const Request = require('./services/request.js');
+const request = new Request('https://restcountries.eu/rest/v2/all');
+
+
+
 const appStart = function(){
-  
+populateList(countries);
 }
 
 
