b83b210e2ca6161b273f3d1ea953dc38a67a1a7b
Angus Malloch <angusbass@hotmail.com>
2018-04-18 15:02:31 +0100

    Added a post method to server.js which will create a country and post to the db. Tested both post and get using insomnia both working.

--- server.js
+++ server.js
@@ -33,7 +33,26 @@ MongoClient.connect("mongodb://localhost:27017", function (err, client) {
     });
   })
 
- // listens at this port
+  app.post("/bucket_list", function(req, res){
+    // we havent created it but as we are accessing it will make it
+    const countriesCollection = db.collection("countries");
+    const countryToSave = req.body;
+
+    countriesCollection.save(countryToSave, function(err, result){
+      if(err){
+        console.log(err);
+        res.status(500);
+        res.send();
+      }
+
+      console.log("Saved to DB!");
+      // create a responce ( good incase it times out! )
+      res.status(201);
+      res.json(countryToSave);
+    })
+  });
+
+  // listens at this port
   app.listen(3000, function(){
     console.log("Listening for requests on port 3000!");
   });
