26ada9fc57fc9088aee2b897e30b9a4a54f97f78
Daniel Conner <dannyconner@hotmail.co.uk>
2018-04-18 15:42:09 +0100

    created countryView File

---
+++ client/src/views/countryView.js
@@ -0,0 +1,24 @@
+const CountryView = function(){
+  this.countries = [];
+};
+
+CountryView.prototype.addCountry = function (country) {
+  this.countries.push(country);
+  this.render(country);
+};
+
+CountryView.prototype.render = function (country) {
+  const ul = document.querySelector('#bucketCountries');
+  const li = document.createElement('li');
+  li.innerText = country.name;
+  ul.appendChild("li")
+};
+
+CountryView.prototype.clear = function () {
+  this.countries = [];
+  const ul = document.querySelector('#bucketCountries');
+  ul.innerHTML = '';
+};
+
+
+module.exports = CountryView;
